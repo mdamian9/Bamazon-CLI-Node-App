1381dd69d870cbad52b68404345ea9d385fd9b37
md_dev <m.damian9@hotmail.com>
2018-07-16 22:42:05 -0700

    Created connection to mySQL, used console.table to display items table properly to console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,35 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 require("console.table");
 
+// Create connection
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "bamazonSeedsDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected as id: " + connection.threadId + "\n");
+});
+
+console.log("Selecting all products...\n");
+connection.query("SELECT * FROM items", function (err, response) {
+    if (err) throw err;
+    console.table(response);
+    connection.end();
+});
+
+
+
 // console.table([
 //     {
 //         name: 'foo',
